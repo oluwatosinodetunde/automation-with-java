9a5eae8a88075332eb7c53ed4e3880174ca348da
Oluwatosin Odetunde <Oluwatosin.Odetunde@Interswitchng.com>
2024-08-19 13:31:08 +0100

    First Commit

---
+++ src/hello/operator.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		
+
+		System.out.println(a +b);
+		//System.out.println("The value is ", c);
+	}
+
+}
